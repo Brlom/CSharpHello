46bcb78f7fbc0566b30bb5f1d75fc4d27e59b2f5
britannia <brlom007@gmail.com>
2020-03-16 20:11:29 +0000

    adding first code on this repo

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = Program.GetGreeting(["Hello", "world"]);
+            //Console.WriteLine(message);
+        }
+
+        static private string GetGreeting(string[] args) {
+            Console.WriteLine(args);
+            //return args;
+        }
+
+    }
+}
